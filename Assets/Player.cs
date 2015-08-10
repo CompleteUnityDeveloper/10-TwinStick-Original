a30aeba8834b0c5416151dabcb6e2dc084f244d1
Ben Tristem <github@bentristem.com>
2015-08-10 19:31:16 +0100

    Grabbing state added, and sort of working.

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	public bool tryingToGrab = false;
+	public bool isGrabbing = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton ("Fire3")) {
+			tryingToGrab = true;
+		} else {
+			tryingToGrab = false;
+			isGrabbing = false;
+		}
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		print (collision.gameObject.tag);
+		if (tryingToGrab && collision.gameObject.tag == "Grabbable") {
+			isGrabbing = true;
+		}
+	}
+
+}
