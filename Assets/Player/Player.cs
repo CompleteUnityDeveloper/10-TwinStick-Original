8c6230745331523e316703a99b2ed23002a06721
Ben Tristem <github@bentristem.com>
2015-08-25 14:44:02 +0100

    TS09 Using The RollerBall Prefab.mp4

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+		Debug.Log ("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
