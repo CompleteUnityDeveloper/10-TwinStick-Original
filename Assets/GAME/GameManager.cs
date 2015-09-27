55b4ee51329d53cb0fb2315cd706cd083f50362e
Ben Tristem <github@bentristem.com>
2015-09-27 13:37:53 +0100

    TS16 Pausing Your Game.mp4

--- Assets/GAME/GameManager.cs
+++ Assets/GAME/GameManager.cs
@@ -6,10 +6,14 @@ public class GameManager : MonoBehaviour {
 
 	public bool recording = true;
 
+	private bool isPaused = false;
+	private float initialFixedDelta;
+
 	void Start () {
 		PlayerPrefsManager.UnlockLevel (2);
 		print (PlayerPrefsManager.IsLevelUnlocked (1));
 		print (PlayerPrefsManager.IsLevelUnlocked (2));
+		initialFixedDelta = Time.fixedDeltaTime;
 	}
 
 	// Update is called once per frame
@@ -19,5 +23,28 @@ public class GameManager : MonoBehaviour {
 		} else {
 			recording = true;
 		}
+
+		if (Input.GetKeyDown (KeyCode.P) && isPaused) {
+			isPaused = false;
+			ResumeGame ();
+		} else if (Input.GetKeyDown (KeyCode.P) && !isPaused) {
+			isPaused = true;
+			PauseGame ();
+		}
+	}
+
+	void PauseGame () {
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+	}
+
+	void ResumeGame () {
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = initialFixedDelta;
+	}
+
+	void OnApplicationPause (bool pauseStatus) {
+		isPaused = pauseStatus;
+		// TODO this will need completing to actually trigger a pause, reader exercise.
 	}
 }
