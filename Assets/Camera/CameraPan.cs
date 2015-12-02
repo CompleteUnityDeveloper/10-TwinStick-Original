760516bf1e30dc93365fc84df50fba6ae5cde80e
Ben Tristem <github@bentristem.com>
2015-12-02 22:44:00 +0000

    Reading From 2nd Stick

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void LateUpdate () {
+		transform.LookAt (player.transform);
+	}
+}
---
+++ Assets/Camera/SelfieStick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfieStick : MonoBehaviour {
+
+	public float panSpeed = 10f;
+
+	private GameObject player;
+	private Vector3 armRotation;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		armRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		armRotation.y += Input.GetAxis ("RHoriz") * panSpeed;
+		armRotation.x += Input.GetAxis ("RVert") * panSpeed;
+
+		transform.position = player.transform.position;
+		transform.rotation = Quaternion.Euler (armRotation);
+	}
+}
